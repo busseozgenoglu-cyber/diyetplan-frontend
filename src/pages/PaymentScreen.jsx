1e627b3f74c999f9e0b7f792f6aab6b6a092cdc0
Aleyna <alen@Aleyna-MBP.home>
2026-03-19 10:38:52 +0300

    add useRef

--- src/pages/PaymentScreen.jsx
+++ src/pages/PaymentScreen.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   ArrowLeft, Lock, CheckCircle, Building2,
